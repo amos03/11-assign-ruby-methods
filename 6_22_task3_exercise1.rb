c92583371b7075f3c69165eaa05ffb8f909f3fd2
amos03 <amossi@gmail.com>
2021-06-22 16:55:29 -0400

    Task 3 Exercise 1.2 cube

--- 6_22_task3_exercise1.rb
+++ 6_22_task3_exercise1.rb
@@ -1,7 +1,13 @@
-#Write a method called multiply_3 that multiplies three numbers together and returns the result.
+#1. Write a method called multiply_3 that multiplies three numbers together and returns the result.
 
 def multiply_3(num1,num2,num3)
 return num1*num2*num3
 end
 
 p multiply_3(10,3,5)
+
+#2. Write a method called cube that takes a number and multiplies it by itself three times. It should return the result.
+def cube(num)
+    return num **3
+end
+p cube(4)
