970ae7135102bdd649d948767a09db4be982a6d0
amos03 <amossi@gmail.com>
2021-06-22 16:52:19 -0400

    Task 3 Exercise 1.1 multiply_3

---
+++ 6_22_task3_exercise1.rb
@@ -0,0 +1,7 @@
+#Write a method called multiply_3 that multiplies three numbers together and returns the result.
+
+def multiply_3(num1,num2,num3)
+return num1*num2*num3
+end
+
+p multiply_3(10,3,5)
